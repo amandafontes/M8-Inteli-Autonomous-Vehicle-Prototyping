2be65b7d80d18fc5140b7a8c0ba28a3dde510dd5
amandafontes <amanda.fontes@sou.inteli.edu.br>
2023-10-30 14:23:48 -0300

    relembrando ros2 com turtlesim

---
+++ ros2/publisher.py
@@ -0,0 +1,48 @@
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import Twist
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = Twist()
+        msg.linear.x = 0.5
+        msg.angular.z = 0.2
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg)
+        self.i += 1
+
+    # def timer_callback(self):
+    #     msg = String()
+    #     msg.data = 'Hello World: %d' % self.i
+    #     self.publisher_.publish(msg)
+    #     self.get_logger().info('Publishing: "%s"' % msg.data)
+    #     self.i += 1
+
+    #     self.publisher_ = self.create_publisher(String, 'topic', 10)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
