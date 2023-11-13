2c44d8ec33e84967a6acd2ff57c5c13f8937632b
amandafontes <amanda.fontes@sou.inteli.edu.br>
2023-11-13 02:10:54 -0300

    ponderada: criação de pacote e launch file para salvar o mapa

---
+++ ponderada/workspace/src/map_saver/launch/launch.py
@@ -0,0 +1,20 @@
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, TimerAction
+
+def generate_launch_description():
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['ros2', 'run', 'nav2_map_server', 'map_saver_cli', '-f', '../maps/mapa'],
+            output='screen',
+        ),
+
+        ExecuteProcess(
+            cmd=['ros2', 'run', 'map_saver', 'script'],
+            output='screen',
+            prefix = 'gnome-terminal --'
+        )
+])
