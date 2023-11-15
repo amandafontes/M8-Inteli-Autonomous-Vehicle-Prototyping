63f9f36bf5c6296f06a0c3db697d372825bc1b86
amandafontes <amanda.fontes@sou.inteli.edu.br>
2023-11-15 04:28:03 -0300

    versão final da atividade de implementação de slam

--- ponderada/workspace/src/navigation/launch/launch.py
+++ ponderada/workspace/src/navigation/launch/launch.py
@@ -1,25 +1,41 @@
+import os
 from launch_ros.actions import Node
 from launch import LaunchDescription
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import ExecuteProcess, IncludeLaunchDescription, TimerAction
 
 def generate_launch_description():
-    return LaunchDescription([
-        ExecuteProcess(
-            cmd=['ros2', 'run', 'turtlebot3_gazebo', 'turtlebot3_world.launch.py'],
-            output='screen',
-        ),
 
-        ExecuteProcess(
-            cmd=['ros2', 'launch', 'turtlebot3_navigation2', 'navigation2.launch.py', 'use_sim_time:=True', 'map:=../../map_saver/maps/map.yaml'],
-            output='screen',
-        ),
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch'),
+            '/turtlebot3_world.launch.py'
+        ])
+    )
 
-        ExecuteProcess(
-            cmd=['ros2', 'run', 'navigation', 'script'],
-            output='screen',
-            prefix = 'gnome-terminal --'
+    rviz = ExecuteProcess(
+            cmd=['ros2', 'launch', 'turtlebot3_navigation2', 'navigation2.launch.py', 'use_sim_time:=True', 'map:=mapa.yaml'],
+            name='navigator_ros2',
+            output='screen'
         )
-])
+
+    delay = ExecuteProcess(
+        cmd=["sleep", "4"],
+        output="screen",
+    )
+
+    waypoints = Node(
+        package='navigation',
+        executable='script',
+        output='screen',
+    )
+
+    return LaunchDescription([
+        gazebo,
+        rviz,
+        delay,
+        waypoints
+    ])
--- ponderada/workspace/src/navigation/navigation/script.py
+++ ponderada/workspace/src/navigation/navigation/script.py
@@ -1,25 +1,45 @@
 #! /usr/bin/env python3
 
 import rclpy
-from nav2_simple_commander.robot_navigator import BasicNavigator
+from math import pi
+import tf_transformations
 from geometry_msgs.msg import PoseStamped
 from tf_transformations import quaternion_from_euler
+from nav2_simple_commander.robot_navigator import BasicNavigator
 
-rclpy.init()
-nav = BasicNavigator()
-q_x, q_y, q_z, q_w = quaternion_from_euler(0.0, 0.0, 0.0)
-initial_pose = PoseStamped()
-initial_pose.header.frame_id = 'map'
-initial_pose.header.stamp = nav.get_clock().now().to_msg()
-initial_pose.pose.position.x = 0.0
-initial_pose.pose.position.y = 0.0
-initial_pose.pose.position.z = 0.0
-initial_pose.pose.orientation.x = q_x
-initial_pose.pose.orientation.y = q_y
-initial_pose.pose.orientation.z = q_z
-initial_pose.pose.orientation.w = q_w
+def create_pose_stamped(navigator, pos_x, pos_y, rot_z):
+    q_x, q_y, q_z, q_w = quaternion_from_euler(0.0, 0.0, rot_z)
+    pose = PoseStamped()
+    pose.header.frame_id = 'map'
+    pose.header.stamp = navigator.get_clock().now().to_msg()
+    pose.pose.position.x = pos_x
+    pose.pose.position.y = pos_y
+    pose.pose.position.z = pos_x
+    pose.pose.orientation.x = q_x
+    pose.pose.orientation.y = q_y
+    pose.pose.orientation.z = q_z
+    pose.pose.orientation.w = q_w
+    return pose
 
-nav.setInitialPose(initial_pose)
-nav.waitUntilNav2Active()
+def main():
+    
+    rclpy.init()
 
-rclpy.shutdown()
+    navigator = BasicNavigator()
+
+    initial_pose = create_pose_stamped(navigator, -1.23, -2.4, 0.0)
+    goal_pose = create_pose_stamped(navigator, 2.5, 2.5, 0.00247)
+
+    waypoints = [initial_pose, goal_pose, initial_pose]
+
+    navigator.waitUntilNav2Active()
+    
+    navigator.followWaypoints(waypoints)
+
+    while not navigator.isTaskComplete():
+        print(navigator.getFeedback())
+
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
