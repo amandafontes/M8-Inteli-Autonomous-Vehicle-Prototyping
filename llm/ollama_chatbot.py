67d43ddd87e65710e5f85afeebb72f5a0d6aaa5a
amandafontes <amanda.fontes@sou.inteli.edu.br>
2023-11-24 02:34:04 -0300

    ponderada 4: chatbot com llm

---
+++ llm/ollama_chatbot.py
@@ -0,0 +1,22 @@
+import gradio as gr
+from langchain.llms import Ollama
+
+model = Ollama(base_url="http://localhost:11434", model="safety-specialist")
+
+def load_model(historico, texto):
+    print('Carregando o modelo...')
+    resposta = model(texto)
+    historico.append(texto, modelo)
+    return "", historico
+
+with gr.Blocks() as interface:
+    title = "Chatbot especialista em segurança do trabalho"
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox(lines=2, placeholder="Digite aqui a sua dúvida sobre segurança do trabalho em ambientes industriais.")
+    submit = gr.Button("Send")
+    clear = gr.ClearButton([msg, chatbot])
+
+    msg.submit(load_model, [chatbot, msg], [msg, chatbot])
+
+if __name__ == "__main__":
+    interface.launch()
